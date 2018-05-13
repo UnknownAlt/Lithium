d0040b8ad5820e2fdea7a9c64c859d5349214e66
PassiveModding <passivemodding@gmail.com>
2018-05-13 19:05:18 +1000

    Added Kicks warns and bans commands

--- Lithium/Modules/Moderation.cs
+++ Lithium/Modules/Moderation.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Lithium.Discord.Contexts;
+using Lithium.Discord.Contexts.Paginator;
 using Lithium.Discord.Extensions;
 using Lithium.Discord.Preconditions;
 using Lithium.Models;
+using Raven.Client.Documents.Smuggler;
+using Serilog.Parsing;
 
 namespace Lithium.Modules
 {
@@ -169,7 +175,7 @@ namespace Lithium.Modules
                 Context.Server.Save();
                 await SendEmbedAsync(new EmbedBuilder
                 {
-                    Title = $"{user.Username} has been banned",
+                    Title = $"{user.Username} has been soft banned",
                     Description = $"User: {user.Username}#{user.Discriminator}\n" +
                                   $"UserID: {user.Id}\n" +
                                   $"Mod: {Context.User.Username}#{Context.User.Discriminator}\n" +
@@ -183,6 +189,145 @@ namespace Lithium.Modules
             {
                 await ReplyAsync("User is unable to be Banned. ");
             }
+        }
+
+
+        [Command("Warns")]
+        [Summary("Warns")]
+        [Remarks("List all logged server warnings")]
+        public async Task Warns()
+        {
+            var pages = new List<PaginatedMessage.Page>();
+            var desc = "";
+            foreach (var warngroup in Context.Server.ModerationSetup.Warns.GroupBy(x => x.userID).ToList())
+            {
+                var first = warngroup.FirstOrDefault();
+                var user = await Context.Client.GetUserAsync(first.userID);
+
+                var dstr = $"**{user.Username ?? first.username} `[{first.userID}]`**\n";
+                foreach (var warn in warngroup)
+                {
+                    dstr += $"Warned By: {(await Context.Client.GetUserAsync(first.modID)).Username ?? warn.modname} `[{warn.modID}]`\n" +
+                            $"Reason: {warn.reason}\n";
+                }
+                dstr += $"-Count: {warngroup.Count()}\n";
+                desc += dstr;
+                if (desc.Length > 800)
+                {
+                    if (desc.Length > 1024)
+                    {
+                        desc = desc.Substring(0, 1023);
+                    }
+                    pages.Add(new PaginatedMessage.Page
+                    {
+                        description = desc
+                    });
+                    desc = "";
+                }
+            }
+            pages.Add(new PaginatedMessage.Page
+            {
+                description = desc
+            });
+            var pager = new PaginatedMessage
+            {
+                Pages = pages,
+                Title = "Warnings",
+                Color = Color.DarkPurple
+            };
+            await PagedReplyAsync(pager);
+        }
+
+        [Command("Kicks")]
+        [Summary("Kicks")]
+        [Remarks("List all logged server Kicks")]
+        public async Task Kicks()
+        {
+            var pages = new List<PaginatedMessage.Page>();
+            var desc = "";
+            foreach (var kickgroup in Context.Server.ModerationSetup.Kicks.GroupBy(x => x.userID).ToList())
+            {
+                var first = kickgroup.FirstOrDefault();
+                var user = await Context.Client.GetUserAsync(first.userID);
+
+                var dstr = $"**{user.Username ?? first.username} `[{first.userID}]`**\n";
+                foreach (var kick in kickgroup)
+                {
+                    dstr += $"Kicked By: {(await Context.Client.GetUserAsync(first.modID)).Username ?? kick.modname} `[{kick.modID}]`\n" +
+                            $"Reason: {kick.reason}\n";
+                }
+                dstr += $"-Count: {kickgroup.Count()}\n";
+                desc += dstr;
+                if (desc.Length > 800)
+                {
+                    if (desc.Length > 1024)
+                    {
+                        desc = desc.Substring(0, 1023);
+                    }
+                    pages.Add(new PaginatedMessage.Page
+                    {
+                        description = desc
+                    });
+                    desc = "";
+                }
+            }
+            pages.Add(new PaginatedMessage.Page
+            {
+                description = desc
+            });
+            var pager = new PaginatedMessage
+            {
+                Pages = pages,
+                Title = "Kicks",
+                Color = Color.DarkMagenta
+            };
+            await PagedReplyAsync(pager);
+        }
+
+        [Command("Bans")]
+        [Summary("Bans")]
+        [Remarks("List all logged server Bans")]
+        public async Task Bans()
+        {
+            var pages = new List<PaginatedMessage.Page>();
+            var desc = "";
+            foreach (var bangroup in Context.Server.ModerationSetup.Bans.GroupBy(x => x.userID).ToList())
+            {
+                var first = bangroup.FirstOrDefault();
+                var user = await Context.Client.GetUserAsync(first.userID);
+
+                var dstr = $"**{user.Username ?? first.username} `[{first.userID}]`**\n";
+                foreach (var ban in bangroup)
+                {
+                    dstr += $"Banned By: {(await Context.Client.GetUserAsync(first.modID)).Username ?? ban.modname} `[{ban.modID}]`\n" +
+                            $"SoftBan: {(ban.Expires ? $"{(ban.ExpiryDate - DateTime.UtcNow).TotalMinutes} Minutes Remaining" : "Permanent Ban")}" +
+                            $"Reason: {ban.reason}\n";
+                }
+                desc += dstr;
+                if (desc.Length > 800)
+                {
+                    if (desc.Length > 1024)
+                    {
+                        desc = desc.Substring(0, 1023);
+                    }
+                    pages.Add(new PaginatedMessage.Page
+                    {
+                        description = desc
+                    });
+                    desc = "";
+                }
+            }
+            pages.Add(new PaginatedMessage.Page
+            {
+                description = desc
+            });
+            var pager = new PaginatedMessage
+            {
+                Pages = pages,
+                Title = "Bans",
+                Color = Color.DarkRed
+            };
+            await PagedReplyAsync(pager);
         }
     }
 }
