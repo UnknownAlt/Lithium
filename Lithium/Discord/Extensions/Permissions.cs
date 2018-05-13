a5b92e01128f0b0e223af036d13c4f5ba5df58fb
PassiveModding <passivemodding@gmail.com>
2018-05-13 16:23:39 +1000

    Check Heirachy Extension
    also added kick and ban commands

---
+++ Lithium/Discord/Extensions/Permissions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Discord;
+using Discord.WebSocket;
+
+namespace Lithium.Discord.Extensions
+{
+    public class Permissions
+    {
+        /// <summary>
+        /// Returns true if the specified user has a higher position (rank) in the guild than the bot
+        /// </summary>
+        /// <param name="targetuser"></param>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        public static bool CheckHeirachy(IGuildUser targetuser, IDiscordClient client)
+        {
+            var guild = targetuser.Guild as SocketGuild;
+            var userrole = guild.Roles.OrderByDescending(x => x.Position).FirstOrDefault(x => targetuser.RoleIds.Contains(x.Id));
+            if (userrole == null)
+            {
+                return false;
+            }
+
+            var gclient = guild.Users.First(x => x.Id == client.CurrentUser.Id);
+            //Here we could also check if the bot has roles, however it should always have a role as upon joining the bot user will have a managed permissions role
+            return userrole.Position > gclient.Roles.Max(x => x.Position);
+        }
+    }
+}
--- Lithium/Modules/Moderation.cs
+++ Lithium/Modules/Moderation.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Lithium.Discord.Contexts;
+using Lithium.Discord.Extensions;
 using Lithium.Discord.Preconditions;
 using Lithium.Models;
 
@@ -11,10 +13,11 @@ namespace Lithium.Modules
     [Group("Mod")]
     public class Moderation : Base
     {
+
         [Command("Warn")]
         [Summary("Warn <@user>")]
         [Remarks("Warn the specified user")]
-        public async Task WarnUser(IUser user, [Remainder] string reason = null)
+        public async Task WarnUser(IGuildUser user, [Remainder] string reason = null)
         {
             Context.Server.ModerationSetup.Warns.Add(new GuildModel.Guild.Moderation.warn
             {
@@ -26,6 +29,110 @@ namespace Lithium.Modules
             });
             await ReplyAsync("User has been warned");
             Context.Server.Save();
+
+            await SendEmbedAsync(new EmbedBuilder
+            {
+                Description = $"User: {user.Username}#{user.Discriminator}\n" +
+                              $"UserID: {user.Id}\n" +
+                              $"Mod: {Context.User.Username}#{Context.User.Discriminator}\n" +
+                              $"Mod ID: {Context.User.Id}\n\n" +
+                              "Reason:\n" +
+                              $"{reason ?? "N/A"}"
+            });
+        }
+
+        [Command("Kick")]
+        [Summary("Kick <@user>")]
+        [Remarks("Kick the specified user")]
+        public async Task KickUser(IGuildUser user, [Remainder] string reason = null)
+        {
+            if (Permissions.CheckHeirachy(user, Context.Client))
+            {
+                await ReplyAsync("This user has higher permissions than me. I cannot perform this action on them");
+                return;
+            }
+
+            if (user.GuildPermissions.Administrator || user.GuildPermissions.KickMembers)
+            {
+                await ReplyAsync("This user has admin or user kick permissions, therefore I cannot perform this action on them");
+                return;
+            }
+
+            Context.Server.ModerationSetup.Kicks.Add(new GuildModel.Guild.Moderation.kick
+            {
+                userID = user.Id,
+                modID = Context.User.Id,
+                modname = Context.User.Username,
+                reason = reason,
+                username = user.Username
+            });
+            try
+            {
+                await user.KickAsync(reason);
+                await ReplyAsync("User has been Kicked");
+                Context.Server.Save();
+                await SendEmbedAsync(new EmbedBuilder
+                {
+                    Title = $"{user.Username} has been kicked",
+                    Description = $"User: {user.Username}#{user.Discriminator}\n" +
+                              $"UserID: {user.Id}\n" +
+                              $"Mod: {Context.User.Username}#{Context.User.Discriminator}\n" +
+                              $"Mod ID: {Context.User.Id}\n\n" +
+                              "Reason:\n" +
+                              $"{reason ?? "N/A"}"
+                });
+            }
+            catch
+            {
+                await ReplyAsync("User is unable to be kicked. ");
+            }
+        }
+
+        [Command("Ban")]
+        [Summary("Ban <@user>")]
+        [Remarks("Ban the specified user")]
+        public async Task BanUser(IGuildUser user, [Remainder] string reason = null)
+        {
+            if (Permissions.CheckHeirachy(user, Context.Client))
+            {
+                await ReplyAsync("This user has higher permissions than me. I cannot perform this action on them");
+                return;
+            }
+
+            if (user.GuildPermissions.Administrator || user.GuildPermissions.BanMembers)
+            {
+                await ReplyAsync("This user has admin or user kick permissions, therefore I cannot perform this action on them");
+                return;
+            }
+
+            Context.Server.ModerationSetup.Kicks.Add(new GuildModel.Guild.Moderation.kick
+            {
+                userID = user.Id,
+                modID = Context.User.Id,
+                modname = Context.User.Username,
+                reason = reason,
+                username = user.Username
+            });
+            try
+            {
+                await Context.Guild.AddBanAsync(user, 1, reason);
+                await ReplyAsync("User has been Banned and messages from the last 24 hours have been cleared");
+                Context.Server.Save();
+                await SendEmbedAsync(new EmbedBuilder
+                {
+                    Title = $"{user.Username} has been banned",
+                    Description = $"User: {user.Username}#{user.Discriminator}\n" +
+                                  $"UserID: {user.Id}\n" +
+                                  $"Mod: {Context.User.Username}#{Context.User.Discriminator}\n" +
+                                  $"Mod ID: {Context.User.Id}\n\n" +
+                                  "Reason:\n" +
+                                  $"{reason ?? "N/A"}"
+                });
+            }
+            catch
+            {
+                await ReplyAsync("User is unable to be Banned. ");
+            }
         }
     }
 }
