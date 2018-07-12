fd21d90382e24d573e564afd5dfbcf43ce23e627
PassiveModding <passivemodding@gmail.com>
2018-07-12 11:45:26 +1000

    Log variants of modlog commands to show expired

--- Lithium/Modules/ModInfo.cs
+++ Lithium/Modules/ModInfo.cs
@@ -41,6 +41,18 @@
         {
             return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.ban, userId);
         }
+                
+        [Command("LongBans")]
+        public Task ViewLongBansAsync(SocketGuildUser user = null)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.ban, user?.Id, true);
+        }
+
+        [Command("LongBans")]
+        public Task ViewLongBansAsync(ulong userId)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.ban, userId, true);
+        }
 
         [Command("Kicks")]
         public Task ViewKicksAsync(SocketGuildUser user = null)
@@ -52,6 +64,18 @@
         public Task ViewKicksAsync(ulong userId)
         {
             return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.Kick, userId);
+        }
+        
+        [Command("LongKicks")]
+        public Task ViewLongKicksAsync(SocketGuildUser user = null)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.Kick, user?.Id, true);
+        }
+
+        [Command("LongKicks")]
+        public Task ViewLongKicksAsync(ulong userId)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.Kick, userId, true);
         }
 
         [Command("Warns")]
@@ -65,6 +89,18 @@
         {
             return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.warn, userId);
         }
+                
+        [Command("LongWarns")]
+        public Task ViewLongWarnsAsync(SocketGuildUser user = null)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.warn, user?.Id, true);
+        }
+
+        [Command("LongWarns")]
+        public Task ViewLongWarnsAsync(ulong userId)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.warn, userId, true);
+        }
 
         [Command("Mutes")]
         public Task ViewMutesAsync(SocketGuildUser user = null)
@@ -76,6 +112,18 @@
         public Task ViewMutesAsync(ulong userId)
         {
             return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.mute, userId);
+        }
+        
+        [Command("LongMutes")]
+        public Task ViewLongMutesAsync(SocketGuildUser user = null)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.mute, user?.Id, true);
+        }
+
+        [Command("LongMutes")]
+        public Task ViewLongMutesAsync(ulong userId)
+        {
+            return GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType.mute, userId, true);
         }
 
         [Command("ModLog")]
@@ -91,9 +139,15 @@
         }
 
         [Command("LongModLog")]
-        public Task ViewLongModLogAsync()
+        public Task ViewLongModLogAsync(SocketGuildUser user = null)
         {
-            return GetModActionsAsync(null, null, true);
+            return GetModActionsAsync(null, user?.Id, true);
+        }
+
+        [Command("LongModLog")]
+        public Task ViewLongModLogAsync(ulong userId)
+        {
+            return GetModActionsAsync(null, userId, true);
         }
 
         public Task GetModActionsAsync(GuildModel.Moderation.ModEvent.EventType? type, ulong? userID = null, bool showExpired = false)
@@ -146,7 +200,6 @@
                         modEvents = Context.Server.ModerationSetup.ModActions.Where(x => x.UserId == userID && x.Action == type && !x.ExpiredOrRemoved).OrderByDescending(x => x.TimeStamp).ToList();
                     }
                 }
-                
             }
 
             var pages = new List<PaginatedMessage.Page>();
