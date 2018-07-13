26a3a878a6668be3bfea1c925e97996f479aefd1
PassiveModding <passivemodding@gmail.com>
2018-07-13 12:02:42 +1000

    CastToSocketTextChannel

---
+++ Lithium/Discord/Extensions/GuildExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Lithium.Discord.Extensions
+{
+    using System;
+
+    using global::Discord.WebSocket;
+
+    public static class GuildExtensions
+    {
+        public static SocketTextChannel CastToSocketTextChannel(this ISocketMessageChannel channel)
+        {
+            if (channel is SocketTextChannel socketTextChannel)
+            {
+                return socketTextChannel;
+            }
+
+            throw new InvalidCastException("The channel cannot be cast to a SocketTextChannel");
+        }
+    }
+}
